4f4d871584c18639541064cbb6627554d22645fe
yharasym <yharasym@class02.training.local>
2018-04-03 12:03:22 +0300

    Add CSVReader; @AfterMethod logging

--- src/test/java/com/softserve/edu/opencart/tests/TestRunner.java
+++ src/test/java/com/softserve/edu/opencart/tests/TestRunner.java
@@ -8,6 +8,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import org.testng.internal.thread.ThreadTimeoutException;
 
 import com.softserve.edu.opencart.data.applications.ApplicationSourceRepository;
 import com.softserve.edu.opencart.pages.Application;
@@ -44,11 +45,31 @@ public abstract class TestRunner {
         logger.info("@BeforeMethod done");
     }
 
-    @AfterMethod//(alwaysRun = true)
-    public void afterMethod(ITestResult result) {
+    @AfterMethod(alwaysRun = true)
+    public void afterMethod(ITestResult testResult) {
         logger.info("@AfterMethod start");
         //Reporter.setCurrentTestResult(result);
         //System.out.println("@AfterMethod");
+        //
+        String message = "\n";
+        if (testResult.getStatus() == ITestResult.SUCCESS) {
+        	message = message + "PASSED: " + testResult.getName();
+        } else {
+        	message = message + "FAILED: " + testResult.getName();
+        	Throwable throwable = testResult.getThrowable();
+			if (throwable != null) {
+				message = message + "\n" + throwable.toString();
+				//
+				// If it's not a thread timeout, include the stack trace too
+				if (!(throwable instanceof ThreadTimeoutException)) {
+					for (StackTraceElement e : throwable.getStackTrace()) {
+						message = message + "\n        at " + e.toString();
+					}
+				}
+			}
+        }
+        //
+        logger.info(message);
         logger.info("@AfterMethod done");
     }
 
